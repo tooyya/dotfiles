6703c4061cfede764397848d5f1b3c0972f26d85
tooyya <ryota.toya@kufu.co.jp>
2025-07-16 13:29:20 +0900

    remove nix

--- home/modules/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Neovim初期化ファイル
--- このファイルはHome Managerによって管理されています
-
--- 基本設定を読み込む
-require('config.options')
-require('config.keymaps')
-
--- lazy.nvimを初期化し、プラグインを管理
-require('config.lazy')
--- home/modules/zsh/default.nix
+++
@@ -1,9 +0,0 @@
-# home/modules/zsh/default.nix
-{ pkgs, ... }:
-
-{
-  # ZSH設定ファイルを配置
-  home.file = {
-    ".zshrc".source = ./.zshrc;
-  };
-}
